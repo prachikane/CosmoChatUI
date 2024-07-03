edc5823e50706718c75aa974d003375598cc5d7a
Prachi Kane <prachikane27@gmail.com>
2024-07-03 16:49:10 -0500

    homepage

---
+++ src/pages/cosmochat.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+const CosmoChat = () =>{
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Welcome to CosmoChat</h1>
+          </header>
+        </div>
+      );
+
+}
+
+export default CosmoChat;
